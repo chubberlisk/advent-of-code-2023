0f0860c26b11a167a3e469fb6d739cba3eac97d6
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-12-27 15:11:14 +0000

    Refactor tests to use Kotest in src/test/kotlin directory

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -14,31 +14,6 @@ class Day01 {
 }
 
 fun main() {
-    // example 1 - it returns calibration value
-    var testInput = readInput("Day01_tdd_test_1")
-    check(Day01.part1(testInput) == 12)
-
-    // example 2 - it returns the first and last digit combined
-    testInput = readInput("Day01_tdd_test_2")
-    check(Day01.part1(testInput) == 13)
-
-    // example 3 -  it ignores letters
-    testInput = readInput("Day01_tdd_test_3")
-    check(Day01.part1(testInput) == 13)
-
-    // example 4 - it handles multiple arty calibration values and returns the sum of their combined first and last digits
-    testInput = readInput("Day01_tdd_test_4")
-    check(Day01.part1(testInput) == 37)
-
-    // example 5 - it handles arty calibration with a single digit only
-    testInput = readInput("Day01_tdd_test_5")
-    check(Day01.part1(testInput) == 48)
-
-    // example 6 - it passes initial tests
-    testInput = readInput("Day01_test")
-    check(Day01.part1(testInput) == 142)
-
-    // example 7 - it gives the correct answer
-    testInput = readInput("Day01")
-    println("Calibration Value: ${Day01.part1(testInput)}")
+    val testInput = readInput("Day01")
+    println("Sum of calibration values: ${Day01.part1(testInput)}")
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -2,7 +2,45 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class Day01Test : DescribeSpec({
-    it("returns a calibration value") {
-        Day01.part1(listOf("12")).shouldBe(12)
+    describe("Part 1") {
+        it("returns the sum of calibration values") {
+            val artyCalibrationValues = listOf("12")
+
+            val sumOfCalibrationValues = Day01.part1(artyCalibrationValues)
+
+            sumOfCalibrationValues.shouldBe(12)
+        }
+
+        it("returns the first and last digit combined") {
+            val artyCalibrationValues = listOf("123")
+
+            val sumOfCalibrationValues = Day01.part1(artyCalibrationValues)
+
+            sumOfCalibrationValues.shouldBe(13)
+        }
+
+        it("ignores letters") {
+            val artyCalibrationValues = listOf("a1abc2xyz3a")
+
+            val sumOfCalibrationValues = Day01.part1(artyCalibrationValues)
+
+            sumOfCalibrationValues.shouldBe(13)
+        }
+
+        it("returns the sum of multiple arty calibration values") {
+            val artyCalibrationValues = listOf("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f")
+
+            val sumOfCalibrationValues = Day01.part1(artyCalibrationValues)
+
+            sumOfCalibrationValues.shouldBe(65)
+        }
+
+        it("returns a single digit calibration value as a two-digit number") {
+            val artyCalibrationValues = listOf("treb7uchet")
+
+            val sumOfCalibrationValues = Day01.part1(artyCalibrationValues)
+
+            sumOfCalibrationValues.shouldBe(77)
+        }
     }
 })
