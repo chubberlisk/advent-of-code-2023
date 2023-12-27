af1fd3b880d7926076ccf9f906f1df203de30000
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-12-27 15:14:10 +0000

    Remove redundant return

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,14 +1,12 @@
 class Day01 {
     companion object {
-        fun part1(artyCalVals: List<String>): Int {
-            return artyCalVals.sumOf { artyCalVal ->
-                val listOfDigits = artyCalVal.filter { it.isDigit() }
+        fun part1(artyCalVals: List<String>): Int = artyCalVals.sumOf { artyCalVal ->
+            val listOfDigits = artyCalVal.filter { it.isDigit() }
 
-                val firstDigit = listOfDigits.first()
-                val lastDigit = listOfDigits.last()
+            val firstDigit = listOfDigits.first()
+            val lastDigit = listOfDigits.last()
 
-                "${firstDigit}${lastDigit}".toInt()
-            }
+            "${firstDigit}${lastDigit}".toInt()
         }
     }
 }
