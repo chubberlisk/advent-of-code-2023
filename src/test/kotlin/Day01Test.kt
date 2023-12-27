cd01b58a4c998b0c4869fd33e67f7c676e665c8e
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-12-27 21:56:41 +0000

    Update multiple calibration values test to use simpler values

--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -36,11 +36,11 @@ class Day01Test : DescribeSpec({
         }
 
         it("returns the sum of multiple arty calibration values") {
-            val artyCalibrationValues = listOf("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f")
+            val artyCalibrationValues = listOf("11", "22", "33")
 
             val sumOfCalibrationValues = Day01.part1(artyCalibrationValues)
 
-            sumOfCalibrationValues.shouldBe(65)
+            sumOfCalibrationValues.shouldBe(66)
         }
     }
 })
