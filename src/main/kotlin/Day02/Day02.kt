169d7a51891b3e413ed0512189b5ad79df3cb4b5
AprilMD <95350189+AprilMD@users.noreply.github.com>
2024-01-17 17:59:38 +0000

    Refactor mapping function to sumOf

--- src/main/kotlin/Day02/Day02.kt
+++ src/main/kotlin/Day02/Day02.kt
@@ -3,11 +3,9 @@ package Day02
 class Day02 {
     companion object {
         fun part1(gameRecord: List<String>): Int {
-            val gameIds = gameRecord.map { game ->
-                gameRecord.indexOf(game) + 1
+            return gameRecord.sumOf { game ->
+                    gameRecord.indexOf(game) + 1
+                }
             }
-
-            return gameIds.sum()
         }
     }
-}
