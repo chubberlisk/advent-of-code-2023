017f043948136b4f52906c7056a08a410dd221a6
AprilMD <95350189+AprilMD@users.noreply.github.com>
2024-01-17 18:39:20 +0000

    Add logic to only sum games below max total cubes

--- src/main/kotlin/Day02/Day02.kt
+++ src/main/kotlin/Day02/Day02.kt
@@ -4,8 +4,21 @@ class Day02 {
     companion object {
         fun part1(gameRecord: List<String>): Int {
             return gameRecord.sumOf { game ->
+                val cubes = game.split(": ").last()
+                val cubeSubsets = cubes.split("; ") // ["15 blue", "14 yellow"]
+                val numberOfCubes = mutableListOf<Int>()
+
+                for (subset in cubeSubsets) {
+                    numberOfCubes.add(subset.split(" ").first().toInt())
+                }
+                val totalCubes = numberOfCubes.sum()
+
+                if(totalCubes <= 39) {
                     gameRecord.indexOf(game) + 1
                 }
+                else 0
             }
         }
     }
+}
+
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -4,12 +4,20 @@ import io.kotest.matchers.shouldBe
 
 class Day02Test: DescribeSpec({
     describe("Part 1: Calculate the sum of the IDs of possible games") {
-        it("Returns the sum of game IDs") {
+        it("returns the sum of game IDs") {
             val gameRecord = listOf("Game 1: 14 blue; 12 red; 13 green", "Game 2: 14 blue; 12 red; 13 green")
 
             val sumOfGameRecord = Day02.part1(gameRecord)
 
             sumOfGameRecord.shouldBe(3)
+        }
+
+        it("returns only counts possible games") {
+            val gameRecord = listOf("Game 1: 15 blue; 12 red; 13 green", "Game 2: 14 blue; 12 red; 13 green")
+
+            val sumOfGameRecord = Day02.part1(gameRecord)
+
+            sumOfGameRecord.shouldBe(2)
         }
     }
 })
